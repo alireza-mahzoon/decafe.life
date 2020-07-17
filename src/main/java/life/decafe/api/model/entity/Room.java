862d5f54b1d65d15d997e18026ec07f516c47f87
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-17 10:55:30 +0200

    add an entity

---
+++ src/main/java/life/decafe/api/model/entity/Room.java
@@ -0,0 +1,27 @@
+package life.decafe.api.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Room {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  private Long id;
+  private Long hotelId;
+  private int number;
+  private String phoneNumber;
+  private int floor;
+  private Long roomTypeId;
+  private LocalDateTime registered;
+  private LocalDateTime updated;
+}
