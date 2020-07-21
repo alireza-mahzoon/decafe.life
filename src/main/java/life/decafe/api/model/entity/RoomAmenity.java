7822d2460a8659ee52a3932913deddf2fe26a2fe
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-21 23:40:02 +0200

    add Schema for room amenity fields

--- src/main/java/life/decafe/api/model/entity/RoomAmenity.java
+++ src/main/java/life/decafe/api/model/entity/RoomAmenity.java
@@ -1,5 +1,6 @@
 package life.decafe.api.model.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,11 +17,18 @@ public class RoomAmenity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
+  @Schema(description = "Unique Id of this room amenity", example = "1233456", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
+  @Schema(description = "Unique Id of this room type", example = "123456")
   private Long roomTypeId;
+  @Schema(description = "Name of room amenity", example = "Fridge")
   private String name;
+  @Schema(description = "More information about room amenity", example = "Beverages provide in the fridge ")
   private String description;
+  @Schema(description = "Price of room amenity", example = "100 SEK")
   private String pricing;
+  @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime registered;
+  @Schema(description = "Updated time and date", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime updated;
 }
