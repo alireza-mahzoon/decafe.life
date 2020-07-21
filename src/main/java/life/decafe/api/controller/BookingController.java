6a4429477c8dcd976df550ebea325d31838bb89a
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-21 15:37:51 +0200

    add a method to create booking

--- src/main/java/life/decafe/api/controller/BookingController.java
+++ src/main/java/life/decafe/api/controller/BookingController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,7 +39,10 @@ public class BookingController {
     return ResponseEntity.notFound().build();
   }
 
-
-
-
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+    LOGGER.info("Creating a booking");
+    Booking bookingCreated = bookingRepository.save(booking);
+    return ResponseEntity.ok(bookingCreated);
+  }
 }
