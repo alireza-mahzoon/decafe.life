de1fd73cb283a60cb0a78fa3a66e8329e4e18d15
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-21 17:31:24 +0200

    add user entity

---
+++ src/main/java/life/decafe/api/model/entity/User.java
@@ -0,0 +1,27 @@
+package life.decafe.api.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private LocalDate birthday;
+  private String email;
+  private LocalDateTime Registered;
+  private LocalDateTime updated;
+}
