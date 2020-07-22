16f15dbbb43e434f8f8982e2a5617faf81fa9a87
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-22 11:27:19 +0200

    add Schema for roomtype fields

--- src/main/java/life/decafe/api/model/entity/RoomType.java
+++ src/main/java/life/decafe/api/model/entity/RoomType.java
@@ -1,5 +1,6 @@
 package life.decafe.api.model.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,11 +17,18 @@ public class RoomType {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
+  @Schema(description = "Unique Id of this room type", example = "1233456", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
+  @Schema(description = "Unique Id for hotel", example = "123456")
   private Long hotelId;
+  @Schema(description = "Name of this room type", example = "Type one")
   private String name;
+  @Schema(description = "Description of this room type", example = "Single bed")
   private String description;
+  @Schema(description = "Capacity of this room type", example = "1")
   private int capacity;
+  @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
+  @Schema(description = "Updated time and date", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Updated;
 }
