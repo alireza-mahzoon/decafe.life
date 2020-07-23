d184f99c355ce5a9c2f9cd6f2cb7052a7859f028
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-23 10:37:07 +0200

    change descriptions in Schema

--- src/main/java/life/decafe/api/model/entity/Profile.java
+++ src/main/java/life/decafe/api/model/entity/Profile.java
@@ -15,15 +15,15 @@ public class Profile {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
-  @Schema(description = "Unique id of this user", example = "123456")
+  @Schema(description = "Unique id of this profile", example = "123456")
   private Long id;
-  @Schema(description = "First name of this user", example = "Peter")
+  @Schema(description = "First name of this profile", example = "Peter")
   private String firstName;
-  @Schema(description = "Last name of this user", example = "Andersson")
+  @Schema(description = "Last name of this profile", example = "Andersson")
   private String lastName;
-  @Schema(description = "Birthday of this user", example = "2000-03-03")
+  @Schema(description = "Birthday of this profile", example = "2000-03-03")
   private LocalDate birthday;
-  @Schema(description = "Email of this user", example = "peter@gmail.com")
+  @Schema(description = "Email of this profile", example = "peter@gmail.com")
   private String email;
   @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
