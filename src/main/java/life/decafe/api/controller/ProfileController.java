8c252b9156041d11eda3b2fc62e24a85ae9b0f30
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-23 10:25:48 +0200

    add a REST API to return profile list

--- src/main/java/life/decafe/api/controller/ProfileController.java
+++ src/main/java/life/decafe/api/controller/ProfileController.java
@@ -1,7 +1,7 @@
 package life.decafe.api.controller;
 
-import life.decafe.api.model.entity.User;
-import life.decafe.api.repository.UserRepository;
+import life.decafe.api.model.entity.Profile;
+import life.decafe.api.repository.ProfileRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +9,24 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Table;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
-public class UserController {
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-  private final UserRepository userRepository;
+@RequestMapping("/profile")
+public class ProfileController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ProfileController.class);
+  private final ProfileRepository profileRepository;
 
   @Autowired
-  public UserController(UserRepository userRepository) {
-    this.userRepository = userRepository;
+  public ProfileController(ProfileRepository profileRepository) {
+    this.profileRepository = profileRepository;
   }
 
-//  @GetMapping(value = "/count", produces = MediaType.APPLICATION_JSON_VALUE)
-//  public ResponseEntity<List<User>> getAllUsers() {
-//    LOGGER.info("Retrieving all users");
-//    List<User> users = userRepository.findAll();
-//    return ResponseEntity.ok(users);
-//  }
-
-  @DeleteMapping(value = "/{userId}")
-  public ResponseEntity<Void> deleteUserById(@PathVariable Long userId) {
-    LOGGER.info("Deleting a user with Id={}", userId);
-    userRepository.deleteById(userId);
-    return ResponseEntity.noContent().build();
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<Profile>> getAllUsers() {
+    LOGGER.info("Retrieving all users");
+    List<Profile> users = profileRepository.findAll();
+    return ResponseEntity.ok(users);
   }
+
 }
