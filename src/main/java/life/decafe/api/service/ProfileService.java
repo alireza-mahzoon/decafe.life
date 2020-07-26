eec54e1641a238a63eb21d3e40fb82f940744a80
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-26 15:48:07 +0200

    add a new method to find profile by Id

--- src/main/java/life/decafe/api/service/ProfileService.java
+++ src/main/java/life/decafe/api/service/ProfileService.java
@@ -3,6 +3,7 @@ package life.decafe.api.service;
 import life.decafe.api.model.entity.Profile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProfileService {
 
@@ -20,4 +21,17 @@ public interface ProfileService {
    */
 
   List<Profile> findAllProfiles();
+
+  /**
+   * Find profile by its Id
+   * @param profileId profile Id
+   * @return profile if exists, empty optional otherwise
+   */
+  Optional<Profile> findProfileById(Long profileId);
+
+
+
+
+
+
 }
