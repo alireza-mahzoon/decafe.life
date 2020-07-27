a3b1f10c3550e34f630bb9c236d0de8698278928
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 18:10:57 +0200

    implement a method to find a method by Id

---
+++ src/main/java/life/decafe/api/service/impl/DefaultRoomAmenityService.java
@@ -0,0 +1,33 @@
+package life.decafe.api.service.impl;
+
+import life.decafe.api.model.entity.RoomAmenity;
+import life.decafe.api.repository.RoomAmenityRepository;
+import life.decafe.api.service.RoomAmenityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DefaultRoomAmenityService implements RoomAmenityService {
+
+  private final Logger LOGGER = LoggerFactory.getLogger(DefaultRoomAmenityService.class);
+  private final RoomAmenityRepository roomAmenityRepository;
+
+  public DefaultRoomAmenityService(RoomAmenityRepository roomAmenityRepository) {
+    this.roomAmenityRepository = roomAmenityRepository;
+  }
+
+  @Override
+  public RoomAmenity createRoomAmenity(RoomAmenity roomAmenity) {
+    LOGGER.debug("Create a room amenity");
+    return roomAmenityRepository.save(roomAmenity);
+  }
+
+  @Override
+  public Optional<RoomAmenity> findRoomAmenityById(Long roomAmenityId) {
+    LOGGER.debug("Find room amenity by Id={}", roomAmenityId);
+    return roomAmenityRepository.findById(roomAmenityId);
+  }
+}
