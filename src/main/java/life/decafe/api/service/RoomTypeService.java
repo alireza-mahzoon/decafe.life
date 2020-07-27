494ffc2bd48cd33472a0639de4852fb5438e12d5
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 16:03:24 +0200

    add a method to find list of room types in a hotel

--- src/main/java/life/decafe/api/service/RoomTypeService.java
+++ src/main/java/life/decafe/api/service/RoomTypeService.java
@@ -2,6 +2,7 @@ package life.decafe.api.service;
 
 import life.decafe.api.model.entity.RoomType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoomTypeService {
@@ -19,4 +20,11 @@ public interface RoomTypeService {
    * @return room type if exists, empty optional otherwise
    */
   Optional<RoomType> findRoomTypeById(Long roomTypeId);
+
+  /**
+   * Find all room types for a hotel Id
+   * @param hotelId
+   * @return list of room types that exist in a hotel
+   */
+  List<RoomType> findAllRoomTypes(Long hotelId);
 }
