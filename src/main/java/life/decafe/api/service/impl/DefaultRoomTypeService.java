fbfcd8cf397725a3743ea101cd3b62b6e80afc4f
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 15:50:58 +0200

    implement a method to find a room type

--- src/main/java/life/decafe/api/service/impl/DefaultRoomTypeService.java
+++ src/main/java/life/decafe/api/service/impl/DefaultRoomTypeService.java
@@ -5,13 +5,17 @@ import life.decafe.api.repository.RoomTypeRepository;
 import life.decafe.api.service.RoomTypeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DefaultRoomTypeService implements RoomTypeService {
-  private final Logger LOGGER = LoggerFactory.getLogger(DefaultRoomService.class);
+  private final Logger LOGGER = LoggerFactory.getLogger(DefaultRoomTypeService.class);
   private final RoomTypeRepository roomTypeRepository;
 
+  @Autowired
   public DefaultRoomTypeService(RoomTypeRepository roomTypeRepository) {
     this.roomTypeRepository = roomTypeRepository;
   }
@@ -20,5 +24,11 @@ public class DefaultRoomTypeService implements RoomTypeService {
   public RoomType createRoomType(RoomType roomType) {
     LOGGER.debug("Create a room type");
     return roomTypeRepository.save(roomType);
+  }
+
+  @Override
+  public Optional<RoomType> findRoomTypeById(Long roomTypeId) {
+    LOGGER.debug("Find a room type by its id={}", roomTypeId);
+    return roomTypeRepository.findById(roomTypeId);
   }
 }
