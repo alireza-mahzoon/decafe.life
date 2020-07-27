e4bbfb19a7ec6b7a4887c07ddc8b48cdc6b4bce3
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 15:50:06 +0200

    add a method to find a room type

--- src/main/java/life/decafe/api/service/RoomTypeService.java
+++ src/main/java/life/decafe/api/service/RoomTypeService.java
@@ -2,6 +2,8 @@ package life.decafe.api.service;
 
 import life.decafe.api.model.entity.RoomType;
 
+import java.util.Optional;
+
 public interface RoomTypeService {
 
   /**
@@ -10,4 +12,11 @@ public interface RoomTypeService {
    * @return newly created roomType
    */
   RoomType createRoomType(RoomType roomType);
+
+  /**
+   * Find a room type by its Id
+   * @param roomTypeId
+   * @return room type if exists, empty optional otherwise
+   */
+  Optional<RoomType> findRoomTypeById(Long roomTypeId);
 }
