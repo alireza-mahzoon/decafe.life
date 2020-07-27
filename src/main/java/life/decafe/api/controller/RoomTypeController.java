0410fb25731996369c67d87822008c92d7a2dfcb
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 16:39:21 +0200

    add a new repository for REST API to delete a room type

--- src/main/java/life/decafe/api/controller/RoomTypeController.java
+++ src/main/java/life/decafe/api/controller/RoomTypeController.java
@@ -66,7 +66,7 @@ public class RoomTypeController {
   @DeleteMapping(value = "/hotel/{hotelId}/roomtype/{roomTypeId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> deleteRoomTypeById(@PathVariable Long hotelId, @PathVariable Long roomTypeId) {
     LOGGER.info("Deleting a roomType with id={}", roomTypeId);
-    roomTypeRepository.deleteById(roomTypeId);
+    roomTypeService.deleteRoomType(roomTypeId);
     return ResponseEntity.noContent().build();
   }
 }
