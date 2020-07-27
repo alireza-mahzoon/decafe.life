a58ab4a2202a45ea23c7eb33230f74b4cf4a8663
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 16:46:29 +0200

    add a method to count room types

--- src/main/java/life/decafe/api/service/RoomTypeService.java
+++ src/main/java/life/decafe/api/service/RoomTypeService.java
@@ -40,6 +40,12 @@ public interface RoomTypeService {
    * @param roomTypeId
    * @return
    */
-
   Void deleteRoomType(Long roomTypeId);
+
+  /**
+   * Count number of all room types
+   * @param
+   * @return number of room types
+   */
+  Long countRoomTypes();
 }
