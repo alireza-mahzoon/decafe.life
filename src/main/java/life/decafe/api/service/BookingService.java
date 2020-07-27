13457d3f43e9506384e31e9a1e1806c54575cefb
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-27 11:23:41 +0200

    add a method to find a booking

--- src/main/java/life/decafe/api/service/BookingService.java
+++ src/main/java/life/decafe/api/service/BookingService.java
@@ -2,6 +2,8 @@ package life.decafe.api.service;
 
 import life.decafe.api.model.entity.Booking;
 
+import java.util.Optional;
+
 public interface BookingService {
 
   /**
@@ -10,4 +12,11 @@ public interface BookingService {
    * @return newly created booking
    */
   Booking createBooking(Booking booking);
+
+  /**
+   * Find a booking by its Id
+   * @param bookingId
+   * @return booking if exists, empty optional otherwise
+   */
+  Optional<Booking> findBookingById(Long bookingId);
 }
