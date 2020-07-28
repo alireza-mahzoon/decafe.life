244b88e7a8a81ed7a8c991d0a8d20f2cbbf39dc2
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-28 11:33:13 +0200

    add a Hotel Dto to be accessed by controller

---
+++ src/main/java/life/decafe/api/model/rest/HotelDto.java
@@ -0,0 +1,30 @@
+package life.decafe.api.model.rest;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class HotelDto {
+  @EqualsAndHashCode.Include
+  @Schema(description = "Unique Id of this hotel", example = "1234567", accessMode = Schema.AccessMode.READ_ONLY)
+  private Long id;
+  @Schema(description = "Hotel name", example = "Scandic")
+  @NotBlank(message = "Name cannot be blank")
+  private String name;
+  @Schema(description = "Address of hotel", example = "Oxford street number 20")
+  @NotBlank(message = "Address cannot be blank")
+  private String address;
+  @Schema(description = "City that hotel is located in", example = "Stockholm")
+  private String city;
+  @Schema(description = "Country that the hotel located", example = "Sweden")
+  private String country;
+  @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
+  private LocalDateTime registered;
+  @Schema(description = "Updated time and date", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
+  private LocalDateTime updated;
+}
