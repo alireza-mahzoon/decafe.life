2d149d1c6b519a2566d9d476e881cd7d63d5c362
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-28 12:14:47 +0200

    add a method to get a lsit of hotel amenities of a hotel

--- src/main/java/life/decafe/api/repository/HotelAmenityRepository.java
+++ src/main/java/life/decafe/api/repository/HotelAmenityRepository.java
@@ -4,6 +4,9 @@ import life.decafe.api.model.entity.HotelAmenity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HotelAmenityRepository extends JpaRepository<HotelAmenity, Long> {
+  List<HotelAmenity> findAllByHotelId(Long hotelId);
 }
