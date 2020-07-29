e82df87c1f599a240d60d9f380759b1110a41fac
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-29 16:00:37 +0200

    edit profile entity

--- src/main/java/life/decafe/api/model/entity/Profile.java
+++ src/main/java/life/decafe/api/model/entity/Profile.java
@@ -1,6 +1,5 @@
 package life.decafe.api.model.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -15,18 +14,11 @@ public class Profile {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
-  @Schema(description = "Unique id of this profile", example = "123456")
   private Long id;
-  @Schema(description = "First name of this profile", example = "Peter")
   private String firstName;
-  @Schema(description = "Last name of this profile", example = "Andersson")
   private String lastName;
-  @Schema(description = "Birthday of this profile", example = "2000-03-03")
   private LocalDate birthday;
-  @Schema(description = "Email of this profile", example = "peter@gmail.com")
   private String email;
-  @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
-  @Schema(description = "Updated time and date", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime updated;
 }
