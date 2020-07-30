4c7ac878271d9d6c545dab39a46e99f5e2b65438
alirezamah <alireza.mahzoon44@gmail.com>
2020-07-30 11:02:54 +0200

    edit booking entity

--- src/main/java/life/decafe/api/model/entity/Booking.java
+++ src/main/java/life/decafe/api/model/entity/Booking.java
@@ -1,6 +1,5 @@
 package life.decafe.api.model.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,20 +17,12 @@ public class Booking {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
-  @Schema(description = "Unique Id of this booking", example = "123456", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
-  @Schema(description = "Unique ID of this user", example = "123456")
   private Long profileId;
-  @Schema(description = "Check in date", example = "2020-09-19")
   private LocalDate checkInDate;
-  @Schema(description = "Check out date", example = "2020-10-19")
   private LocalDate checkOutDate;
-  @Schema(description = "Unique Id for this hotel", example = "123456")
   private Long hotelId;
-  @Schema(description = "Unique Id for this room", example = "123456")
   private Long roomId;
-  @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
-  @Schema(description = "Updated time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Updated;
 }
