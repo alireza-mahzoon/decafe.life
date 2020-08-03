8a78193532ebf16cd364fa67bcf3fad41ec19cb8
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-03 23:06:14 +0200

    add a method to room repository to find a room

--- src/main/java/life/decafe/api/repository/RoomRepository.java
+++ src/main/java/life/decafe/api/repository/RoomRepository.java
@@ -1,12 +1,15 @@
 package life.decafe.api.repository;
 
+import life.decafe.api.model.entity.Profile;
 import life.decafe.api.model.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
   List<Room> findAllByHotelId(Long hotelId);
+  Optional<Room> findRoomByHotelIdAndNumber(Long hotelId, Integer number);
 }
