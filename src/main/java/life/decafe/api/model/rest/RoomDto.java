7a3a67eb8b0aba42cd450f4e84341f6a0d13e7fa
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-04 22:52:11 +0200

    add validation for roomDto

--- src/main/java/life/decafe/api/model/rest/RoomDto.java
+++ src/main/java/life/decafe/api/model/rest/RoomDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,6 +14,7 @@ public class RoomDto {
   @Schema(description = "Unique Id of this room", example = "123456", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
   @Schema(description = "Unique Id of this hotel", example = "1223456")
+  @NotNull
   private Long hotelId;
   @Schema(description = "Room Number", example = "12345")
   @NotBlank(message = "Number cannot be blank")
@@ -23,8 +24,10 @@ public class RoomDto {
   private String phoneNumber;
   @Schema(description = "Room floor", example = "23")
   @NotBlank(message = "Floor can not be blank")
+  @PositiveOrZero
   private int floor;
   @Schema(description = "Unique Id of this roomType", example = "123456")
+  @NotNull
   private Long roomTypeId;
   @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime registered;
