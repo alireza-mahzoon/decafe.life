b503853888b85bfb7a8ea1cd94c62dca18b24dac
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-06 14:50:34 +0200

    add validation for a method to create hotel

--- src/main/java/life/decafe/api/service/impl/DefaultHotelService.java
+++ src/main/java/life/decafe/api/service/impl/DefaultHotelService.java
@@ -1,5 +1,6 @@
 package life.decafe.api.service.impl;
 
+import life.decafe.api.exception.BadRequestException;
 import life.decafe.api.model.entity.Hotel;
 import life.decafe.api.model.mapper.BeanMapper;
 import life.decafe.api.model.rest.HotelDto;
@@ -10,9 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DefaultHotelService implements HotelService {
@@ -21,7 +23,7 @@ public class DefaultHotelService implements HotelService {
   private final BeanMapper beanMapper;
 
   @Autowired
-  public DefaultHotelService(HotelRepository hotelRepository, BeanMapper beanMapper, HotelService hotelService){
+  public DefaultHotelService(HotelRepository hotelRepository, BeanMapper beanMapper){
     this.hotelRepository = hotelRepository;
     this.beanMapper = beanMapper;
   }
@@ -29,6 +31,12 @@ public class DefaultHotelService implements HotelService {
   @Override
   public HotelDto createHotel(HotelDto hotel) {
     LOGGER.debug("Create a hotel");
+    hotel.setId(null);
+    hotel.setRegistered(LocalDateTime.now());
+    hotel.setUpdated(hotel.getRegistered());
+    if (hotelRepository.findByAddress(hotel.getAddress()).isPresent()) {
+      throw new BadRequestException("The hotel already created");
+    }
     Hotel hotelCreated = hotelRepository.save(beanMapper.map(hotel));
     return beanMapper.map(hotelCreated);
   }
@@ -36,20 +44,14 @@ public class DefaultHotelService implements HotelService {
   @Override
   public Optional<HotelDto> findHotelById(Long hotelId) {
     LOGGER.debug("Find a hotel by Id={}", hotelId);
-    Optional<Hotel> hotel = hotelRepository.findById(hotelId);
-    Optional<HotelDto> hotelDto = Optional.of(beanMapper.map(hotel.get()));
-    return hotelDto;
+    return hotelRepository.findById(hotelId).map(beanMapper::map);
   }
 
   @Override
   public List<HotelDto> findAllHotels() {
     LOGGER.debug("Find all hotels");
     List<Hotel> hotels = hotelRepository.findAll();
-    List<HotelDto> hotelDtos = new ArrayList<>();
-    for (Hotel hotel: hotels) {
-      hotelDtos.add(beanMapper.map(hotel));
-    }
-    return hotelDtos;
+    return hotels.stream().map(beanMapper::map).collect(Collectors.toList());
   }
 
   @Override
