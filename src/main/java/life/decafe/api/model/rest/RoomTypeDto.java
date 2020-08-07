2be999a1a6dbfd03819f00222b44f6e883eccde9
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-07 18:30:43 +0200

    add validation to roomtype fields

--- src/main/java/life/decafe/api/model/rest/RoomTypeDto.java
+++ src/main/java/life/decafe/api/model/rest/RoomTypeDto.java
@@ -4,7 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,7 +17,7 @@ public class RoomTypeDto {
   @Schema(description = "Unique Id of this room type", example = "1233456", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
   @Schema(description = "Unique Id for hotel", example = "123456")
-  @NotBlank(message = "HotelId cannot be blank")
+  @NotNull
   private Long hotelId;
   @Schema(description = "Name of this room type", example = "Type one")
   @NotBlank(message = "Name cannot be blank")
@@ -24,7 +27,8 @@ public class RoomTypeDto {
   private String description;
   @Schema(description = "Capacity of this room type", example = "1")
   @NotBlank(message = "Capacity can not be blank")
-  private int capacity;
+  @Min(value = 1, message = "The value must be positive")
+  private Integer capacity;
   @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
   @Schema(description = "Updated time and date", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
