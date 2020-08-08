8f3f830a63ec4af6d3b8e73bfb99c46ad7ddf62b
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-08 23:53:39 +0200

    fix validation for firelds

--- src/main/java/life/decafe/api/model/rest/ProfileDto.java
+++ src/main/java/life/decafe/api/model/rest/ProfileDto.java
@@ -3,8 +3,9 @@ package life.decafe.api.model.rest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -21,9 +22,12 @@ public class ProfileDto {
   @NotBlank(message ="LastName can not be blank")
   private String lastName;
   @Schema(description = "Birthday of this profile", example = "2000-03-03")
+  @NotNull
+  @Past
   private LocalDate birthday;
   @Schema(description = "Email of this profile", example = "peter@gmail.com")
-  @NotBlank(message = "Email cannot be blank")
+  @Email(message = "Email cannot be blank")
+  @NotNull(message = "The email is not entered")
   private String email;
   @Schema(description = "Registered time and date of data", example = "2020-03-03 19:20:20.994", accessMode = Schema.AccessMode.READ_ONLY)
   private LocalDateTime Registered;
