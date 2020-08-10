45fcd88ddad41bea57e527f6ef5efb49769e8b38
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-10 11:14:29 +0200

    fix a method to delete a room amenity

--- src/main/java/life/decafe/api/repository/RoomAmenityRepository.java
+++ src/main/java/life/decafe/api/repository/RoomAmenityRepository.java
@@ -1,12 +1,15 @@
 package life.decafe.api.repository;
 
+import life.decafe.api.model.entity.Room;
 import life.decafe.api.model.entity.RoomAmenity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomAmenityRepository extends JpaRepository<RoomAmenity, Long> {
   List<RoomAmenity> findAllByRoomTypeId(Long RoomTypeId);
+  Optional<RoomAmenity> findByRoomTypeIdAndName(Long RoomTypeId, String name);
 }
