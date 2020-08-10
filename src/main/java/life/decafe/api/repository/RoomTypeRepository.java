c4fd9399f13e19bf05b35b4d06dcdc59443c31ea
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-10 19:05:43 +0200

    fix a method to delete roomtype

--- src/main/java/life/decafe/api/repository/RoomTypeRepository.java
+++ src/main/java/life/decafe/api/repository/RoomTypeRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomTypeRepository extends JpaRepository<RoomType, Long> {
   List<RoomType> findAllByHotelId(Long hotelId);
+  boolean existsByIdAndHotelId(Long id, Long hotelId);
+  Optional<RoomType> findByHotelIdAndName(Long hotelId, String name);
 }
