22f023727f730d65af59f247a600659c8482c10c
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-10 18:07:50 +0200

    add validation for a method to create room type

--- src/main/java/life/decafe/api/service/impl/DefaultRoomTypeService.java
+++ src/main/java/life/decafe/api/service/impl/DefaultRoomTypeService.java
@@ -1,8 +1,11 @@
 package life.decafe.api.service.impl;
 
+import life.decafe.api.exception.NotFoundException;
+import life.decafe.api.exception.ResourceConflictException;
 import life.decafe.api.model.entity.RoomType;
 import life.decafe.api.model.mapper.BeanMapper;
 import life.decafe.api.model.rest.RoomTypeDto;
+import life.decafe.api.repository.HotelRepository;
 import life.decafe.api.repository.RoomTypeRepository;
 import life.decafe.api.service.RoomTypeService;
 import org.slf4j.Logger;
@@ -10,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,17 +22,29 @@ import java.util.stream.Collectors;
 public class DefaultRoomTypeService implements RoomTypeService {
   private final Logger LOGGER = LoggerFactory.getLogger(DefaultRoomTypeService.class);
   private final RoomTypeRepository roomTypeRepository;
+  private final HotelRepository hotelRepository;
   private final BeanMapper beanMapper;
 
   @Autowired
-  public DefaultRoomTypeService(RoomTypeRepository roomTypeRepository, BeanMapper beanMapper) {
+  public DefaultRoomTypeService(RoomTypeRepository roomTypeRepository, HotelRepository hotelRepository, BeanMapper beanMapper) {
     this.roomTypeRepository = roomTypeRepository;
+    this.hotelRepository = hotelRepository;
     this.beanMapper = beanMapper;
   }
 
   @Override
   public RoomTypeDto createRoomType(RoomTypeDto roomType) {
     LOGGER.debug("Create a room type");
+    roomType.setId(null);
+    roomType.setRegistered(LocalDateTime.now());
+    roomType.setUpdated(roomType.getRegistered());
+    if (!hotelRepository.existsById(roomType.getHotelId())) {
+      throw new NotFoundException("The hotel Id is not existed");
+    }
+    Optional<RoomType> existedRoomType = roomTypeRepository.findByHotelIdAndName(roomType.getHotelId(), roomType.getName());
+    if (existedRoomType.isPresent()) {
+      throw new ResourceConflictException("RoomType already exists");
+    }
     RoomType roomTypeCreated = roomTypeRepository.save(beanMapper.map(roomType));
     return beanMapper.map(roomTypeCreated);
   }
