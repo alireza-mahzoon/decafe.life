e25ffbaec55ea093749fda8cee7e5deae8a0b258
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-10 12:43:24 +0200

    fix a method to delete hotel amenity

--- src/main/java/life/decafe/api/repository/HotelAmenityRepository.java
+++ src/main/java/life/decafe/api/repository/HotelAmenityRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HotelAmenityRepository extends JpaRepository<HotelAmenity, Long> {
   List<HotelAmenity> findAllByHotelId(Long hotelId);
+  Optional<HotelAmenity> findByHotelIdAndName(Long hotelId, String name);
 }
--- src/main/java/life/decafe/api/service/impl/DefaultHotelAmenityService.java
+++ src/main/java/life/decafe/api/service/impl/DefaultHotelAmenityService.java
@@ -1,14 +1,19 @@
 package life.decafe.api.service.impl;
 
+import life.decafe.api.exception.NotFoundException;
+import life.decafe.api.exception.ResourceConflictException;
 import life.decafe.api.model.entity.HotelAmenity;
+import life.decafe.api.model.entity.RoomAmenity;
 import life.decafe.api.model.mapper.BeanMapper;
 import life.decafe.api.model.rest.HotelAmenityDto;
 import life.decafe.api.repository.HotelAmenityRepository;
+import life.decafe.api.repository.HotelRepository;
 import life.decafe.api.service.HotelAmenityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,16 +22,28 @@ import java.util.stream.Collectors;
 public class DefaultHotelAmenityService implements HotelAmenityService {
   private final Logger LOGGER = LoggerFactory.getLogger(DefaultHotelAmenityService.class);
   private final HotelAmenityRepository hotelAmenityRepository;
+  private final HotelRepository hotelRepository;
   private final BeanMapper beanMapper;
 
-  public DefaultHotelAmenityService(HotelAmenityRepository hotelAmenityRepository, BeanMapper beanMapper) {
+  public DefaultHotelAmenityService(HotelAmenityRepository hotelAmenityRepository, HotelRepository hotelRepository, BeanMapper beanMapper) {
     this.hotelAmenityRepository = hotelAmenityRepository;
+    this.hotelRepository = hotelRepository;
     this.beanMapper = beanMapper;
   }
 
   @Override
   public HotelAmenityDto createHotelAmenity(HotelAmenityDto hotelAmenity) {
     LOGGER.debug("Create a hotel amenity");
+    hotelAmenity.setId(null);
+    hotelAmenity.setRegistered(LocalDateTime.now());
+    hotelAmenity.setUpdated(hotelAmenity.getRegistered());
+    if (!hotelRepository.existsById(hotelAmenity.getHotelId())) {
+      throw new NotFoundException("This hotel id is not existed");
+    }
+    Optional<HotelAmenity> existedHotelAmenity = hotelAmenityRepository.findByHotelIdAndName(hotelAmenity.getHotelId(), hotelAmenity.getName());
+    if (existedHotelAmenity.isPresent()) {
+      throw new ResourceConflictException("This amenity is already exists");
+    }
     HotelAmenity hotelAmenityCreated = hotelAmenityRepository.save(beanMapper.map(hotelAmenity));
     return beanMapper.map(hotelAmenityCreated);
   }
@@ -52,7 +69,7 @@ public class DefaultHotelAmenityService implements HotelAmenityService {
   }
 
   @Override
-  public Void deleteHotelAmenityById(Long hotelAmenityId) {
+  public void deleteHotelAmenityById(Long hotelAmenityId) {
     LOGGER.debug("Delete hotel amenity with Id={}", hotelAmenityId);
     hotelAmenityRepository.deleteById(hotelAmenityId);
     return null;
