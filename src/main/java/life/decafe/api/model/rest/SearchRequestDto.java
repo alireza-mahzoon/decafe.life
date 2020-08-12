20f9d6cbfc2a835300f95f58dec7ec13c21411fe
alirezamah <alireza.mahzoon44@gmail.com>
2020-08-13 01:27:47 +0200

    add SearchRequestResponseDto

---
+++ src/main/java/life/decafe/api/model/rest/SearchRequestDto.java
@@ -0,0 +1,9 @@
+package life.decafe.api.model.rest;
+
+import java.time.LocalDate;
+
+public class SearchRequestDto {
+  private String city;
+  private LocalDate checkInDate;
+  private LocalDate checkOutDate;
+}
---
+++ src/main/java/life/decafe/api/model/rest/SearchResponseDto.java
@@ -0,0 +1,11 @@
+package life.decafe.api.model.rest;
+
+import life.decafe.api.model.entity.Hotel;
+
+import java.util.List;
+
+public class SearchResponseDto {
+  List<Hotel> hotelList;
+
+
+}
